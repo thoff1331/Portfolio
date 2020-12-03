f5509438de55d0f9ae35d255367a80e2bd8694b3
thoff1331 <trevorhoffman@Trevors-MacBook-Air.local>
2020-12-03 11:00:50 -0700

    skills page constructed

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -1,9 +1,20 @@
 import React from "react";
+import "../Skills/skills.css";
+import skills from "../Data/logos.json";
 
 function Skills() {
   return (
-    <div>
-      <h1>These Are My Skills</h1>
+    <div className="skills-container">
+      <div className="skills-elements">
+        {skills.map((skill, index) => {
+          return (
+            <div className="image-with-label" key={index}>
+              <label>{skill.label}</label>
+              <img src={skill.src} />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
